8ea04576f4f486b3d61c989393e383e7eebcdef6
jacek <jacek@PIECYK11>
2023-06-18 19:59:52 +0200

    .

--- WinFormOpenCVRec2/Form1.Designer.cs
+++ WinFormOpenCVRec2/Form1.Designer.cs
@@ -32,6 +32,13 @@
             buttonStart = new Button();
             buttonStop = new Button();
             buttonChangeCamera = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label21 = new Label();
+            cbRozdzielczoscVideo = new ComboBox();
+            cbFormatVideo = new ComboBox();
+            label22 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -41,13 +48,13 @@
             pictureBox1.Dock = DockStyle.Top;
             pictureBox1.Location = new Point(0, 0);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1980, 990);
+            pictureBox1.Size = new Size(1393, 1308);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // buttonStart
             // 
-            buttonStart.Location = new Point(12, 996);
+            buttonStart.Location = new Point(34, 1347);
             buttonStart.Name = "buttonStart";
             buttonStart.Size = new Size(112, 34);
             buttonStart.TabIndex = 1;
@@ -57,7 +64,8 @@
             // 
             // buttonStop
             // 
-            buttonStop.Location = new Point(130, 996);
+            buttonStop.Enabled = false;
+            buttonStop.Location = new Point(166, 1347);
             buttonStop.Name = "buttonStop";
             buttonStop.Size = new Size(112, 34);
             buttonStop.TabIndex = 2;
@@ -67,7 +75,7 @@
             // 
             // buttonChangeCamera
             // 
-            buttonChangeCamera.Location = new Point(270, 1000);
+            buttonChangeCamera.Location = new Point(284, 1347);
             buttonChangeCamera.Name = "buttonChangeCamera";
             buttonChangeCamera.Size = new Size(112, 34);
             buttonChangeCamera.TabIndex = 3;
@@ -75,22 +83,101 @@
             buttonChangeCamera.UseVisualStyleBackColor = true;
             buttonChangeCamera.Click += buttonChangeCamera_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1058, 1355);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 30);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1166, 1355);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 30);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1267, 1355);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 30);
+            label3.TabIndex = 6;
+            label3.Text = "label3";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(465, 1311);
+            label21.Margin = new Padding(5, 0, 5, 0);
+            label21.Name = "label21";
+            label21.Size = new Size(200, 30);
+            label21.TabIndex = 4;
+            label21.Text = "Rozdzielczość Video";
+            // 
+            // cbRozdzielczoscVideo
+            // 
+            cbRozdzielczoscVideo.FormattingEnabled = true;
+            cbRozdzielczoscVideo.Items.AddRange(new object[] { "640 x 480 (VGA)", "1280 x 720 (HD)", "1920 x 1080 (FHD)" });
+            cbRozdzielczoscVideo.Location = new Point(465, 1347);
+            cbRozdzielczoscVideo.Margin = new Padding(5, 6, 5, 6);
+            cbRozdzielczoscVideo.Name = "cbRozdzielczoscVideo";
+            cbRozdzielczoscVideo.Size = new Size(189, 38);
+            cbRozdzielczoscVideo.TabIndex = 5;
+            cbRozdzielczoscVideo.SelectedIndexChanged += cbRozdzielczoscVideo_SelectedIndexChanged;
+            // 
+            // cbFormatVideo
+            // 
+            cbFormatVideo.FormattingEnabled = true;
+            cbFormatVideo.Items.AddRange(new object[] { "H265", "MPG4", "HEVC" });
+            cbFormatVideo.Location = new Point(717, 1347);
+            cbFormatVideo.Margin = new Padding(5, 6, 5, 6);
+            cbFormatVideo.Name = "cbFormatVideo";
+            cbFormatVideo.Size = new Size(205, 38);
+            cbFormatVideo.TabIndex = 6;
+            cbFormatVideo.SelectedIndexChanged += cbFormatVideo_SelectedIndexChanged;
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.Location = new Point(751, 1311);
+            label22.Margin = new Padding(5, 0, 5, 0);
+            label22.Name = "label22";
+            label22.Size = new Size(137, 30);
+            label22.TabIndex = 7;
+            label22.Text = "Format Video";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1980, 1204);
+            ClientSize = new Size(1393, 1393);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(buttonChangeCamera);
             Controls.Add(buttonStop);
             Controls.Add(buttonStart);
             Controls.Add(pictureBox1);
+            Controls.Add(label22);
+            Controls.Add(cbFormatVideo);
+            Controls.Add(cbRozdzielczoscVideo);
+            Controls.Add(label21);
             Name = "Form1";
             Text = "Form1";
             FormClosing += Form1_FormClosing;
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
+
+
 
         #endregion
 
@@ -98,5 +185,12 @@
         private Button buttonStart;
         private Button buttonStop;
         private Button buttonChangeCamera;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label21;
+        private ComboBox cbRozdzielczoscVideo;
+        private ComboBox cbFormatVideo;
+        private Label label22;
     }
 }
--- WinFormOpenCVRec2/Form1.cs
+++ WinFormOpenCVRec2/Form1.cs
@@ -5,6 +5,9 @@ using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace WinFormOpenCVRec2
 {
@@ -13,16 +16,24 @@ namespace WinFormOpenCVRec2
 
         private System.ComponentModel.BackgroundWorker backgroundWorkerDisplayer;
         private System.ComponentModel.BackgroundWorker backgroundWorkerRecorder;
-        private  VideoCapture capture;
+        private VideoCapture capture;
         private VideoWriter videoWriter;
         private int currentCamera = 0;
         private bool camerachanging = false;
+        private bool restartDisplayer = false;
+
+
+        FourCC fourCC;
+        VideoCaptureAPIs videoCaptureApi;
+        int frameWidth;
+        int frameHeight;
         public Form1()
         {
             InitializeComponent();
             InitBackgroundWorkers();
 
-            capture = new VideoCapture();
+            cbRozdzielczoscVideo.SelectedIndex = 0;
+            cbFormatVideo.SelectedIndex = 0;
 
         }
 
@@ -35,16 +46,48 @@ namespace WinFormOpenCVRec2
             backgroundWorkerDisplayer.DoWork += BackgroundWorkerDisplayer_DoWork;
             backgroundWorkerDisplayer.WorkerReportsProgress = true;
             backgroundWorkerDisplayer.ProgressChanged += BackgroundWorkerDisplayer_ProgressChanged;
+            backgroundWorkerDisplayer.RunWorkerCompleted += BackgroundWorkerDisplayer_RunWorkerCompleted;
 
             backgroundWorkerRecorder.WorkerSupportsCancellation = true;
             backgroundWorkerRecorder.DoWork += BackgroundWorkerRecorder_DoWork;
+            backgroundWorkerRecorder.RunWorkerCompleted += BackgroundWorkerRecorder_RunWorkerCompleted;
+        }
+
+        private void BackgroundWorkerDisplayer_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            InvokeControlText(label2, "Zakonczono wyswietlanie");
+
+            capture.Dispose();
+            capture = null;
+
+            if (restartDisplayer)
+            {
+
+
+                InitCamera();
+
+                if (!capture.IsOpened())
+                {
+                    Close();
+                    return;
+                }
+                restartDisplayer = false;
+                backgroundWorkerDisplayer.RunWorkerAsync();
+            }
+        }
+
+        private void BackgroundWorkerRecorder_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            videoWriter?.Dispose();
+            videoWriter = null;
+            InvokeControlText(label2, "Zakonczono nagrywanie");
         }
 
         private void DisposeBackgroundWorkers()
         {
             backgroundWorkerDisplayer.CancelAsync();
             backgroundWorkerRecorder.CancelAsync();
-        
+
             backgroundWorkerDisplayer.DoWork -= BackgroundWorkerDisplayer_DoWork;
             backgroundWorkerDisplayer.ProgressChanged -= BackgroundWorkerDisplayer_ProgressChanged;
             backgroundWorkerRecorder.DoWork -= BackgroundWorkerRecorder_DoWork;
@@ -66,7 +109,7 @@ namespace WinFormOpenCVRec2
         private void BackgroundWorkerRecorder_DoWork(object? sender, System.ComponentModel.DoWorkEventArgs e)
         {
             if (capture is null || videoWriter is null || camerachanging) return;
-            
+
             var bgWorker = (BackgroundWorker)sender;
 
             try
@@ -81,22 +124,27 @@ namespace WinFormOpenCVRec2
                     lastWrite = DateTime.Now;
                     using (var frameMat = capture.RetrieveMat())
                     {
-                        if (frameMat != null && videoWriter != null)
+                        if (frameMat != null && videoWriter != null && !frameMat.Empty())
                         {
                             videoWriter.Write(frameMat);
                         }
                     }
+
+                    InvokeControlText(label2, DateTime.Now.ToString());
+
                 }
             }
             catch (Exception ex)
             {
                 throw;
             }
+
+
         }
 
         private void BackgroundWorkerDisplayer_DoWork(object? sender, System.ComponentModel.DoWorkEventArgs e)
         {
-            if (capture is null  || camerachanging) return; 
+            if (capture is null || camerachanging) return;
 
             var bgWorker = (BackgroundWorker)sender;
 
@@ -106,10 +154,12 @@ namespace WinFormOpenCVRec2
                 {
                     try
                     {
-                        if (frameMat != null)
+                        if (frameMat != null && !frameMat.Empty())
                         {
                             var frameBitmap = BitmapConverter.ToBitmap(frameMat);
-                            bgWorker.ReportProgress(0, frameBitmap); 
+
+                            bgWorker.ReportProgress(0, frameBitmap);
+
                         }
                     }
                     catch (Exception)
@@ -119,14 +169,18 @@ namespace WinFormOpenCVRec2
                     }
                 }
                 Thread.Sleep(100);
+
+                InvokeControlText(label1, DateTime.Now.ToString());
             }
+
         }
 
 
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            capture.Open(1, VideoCaptureAPIs.ANY);
+            InitCamera();
+
             if (!capture.IsOpened())
             {
                 Close();
@@ -139,11 +193,21 @@ namespace WinFormOpenCVRec2
 
         }
 
+        private void InitCamera()
+        {
+            capture = new OpenCvSharp.VideoCapture(); //VideoCapture.FromCamera(deviceIndex, _videoCaptureApi);
+            capture.Open(currentCamera, videoCaptureApi);
+
+            capture.FrameWidth = frameWidth;
+            capture.FrameHeight = frameHeight;
+            capture.Fps = 29;
+        }
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             backgroundWorkerDisplayer.CancelAsync();
             backgroundWorkerRecorder.CancelAsync();
-         
+
             videoWriter?.Release();
             videoWriter?.Dispose();
             videoWriter = null;
@@ -153,63 +217,120 @@ namespace WinFormOpenCVRec2
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
-          //  StopRecording();
-          //  Thread.Yield();
             StartRecording();
-
         }
 
         private void StartRecording()
         {
             var path = $"file{DateTime.Now.Ticks}.mp4";
-            videoWriter = new VideoWriter(path, FourCC.HEVC, capture.Fps, new OpenCvSharp.Size(capture.FrameWidth, capture.FrameHeight));
 
-            backgroundWorkerRecorder.RunWorkerAsync();
-        }
 
-        private void buttonStop_Click(object sender, EventArgs e)
-        {
-            StopRecording();
-        }
-
-        private void StopRecording()
-        {
-            backgroundWorkerRecorder.CancelAsync();
-         
-            videoWriter?.Release();
-            videoWriter?.Dispose();
-            videoWriter = null;
-        }
-
-        private void buttonChangeCamera_Click(object sender, EventArgs e)
-        {
-
-            backgroundWorkerDisplayer.CancelAsync();
-            backgroundWorkerRecorder.CancelAsync();
-
-            DisposeBackgroundWorkers();
-
-            camerachanging = true;
-
-            capture.Dispose();
-            capture = null;
             capture = new VideoCapture();
 
-            currentCamera = currentCamera == 0 ? 1 : 0;
-
-
-            var res = capture.Open(currentCamera, VideoCaptureAPIs.ANY);
+            var res = capture.Open(1, VideoCaptureAPIs.ANY);
             if (!capture.IsOpened())
             {
                 Close();
                 return;
             }
-            camerachanging = false;
 
-            InitBackgroundWorkers();
+            videoWriter = new VideoWriter(path, fourCC, capture.Fps, new OpenCvSharp.Size(frameWidth, frameHeight));
+
+            backgroundWorkerRecorder.RunWorkerAsync();
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            StopRecording();
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void StopRecording()
+        {
+            backgroundWorkerRecorder.CancelAsync();
+
+
+            videoWriter?.Release();
 
         }
 
+        private void buttonChangeCamera_Click(object sender, EventArgs e)
+        {
+            backgroundWorkerRecorder.CancelAsync();
+            restartDisplayer = true;
+            backgroundWorkerDisplayer.CancelAsync();
 
+
+            currentCamera = currentCamera == 0 ? 1 : 0;
+            label3.Text = $"current camera {currentCamera}";
+        }
+
+
+        public static void InvokeControlText<T>(Control control, T e)
+        {
+            if (control.InvokeRequired)
+            {
+                control.BeginInvoke((MethodInvoker)delegate ()
+                {
+                    control.Text = e.ToString();
+                });
+            }
+            else
+            {
+                control.Text = e.ToString();
+            }
+        }
+
+        private void cbRozdzielczoscVideo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbRozdzielczoscVideo.SelectedIndex == 0)
+            {
+                videoCaptureApi = VideoCaptureAPIs.DSHOW;
+                frameWidth = 640;
+                frameHeight = 480;
+            }
+            else if (cbRozdzielczoscVideo.SelectedIndex == 1)
+            {
+                videoCaptureApi = VideoCaptureAPIs.ANY;
+                frameWidth = 1280;
+                frameHeight = 720;
+            }
+            else if (cbRozdzielczoscVideo.SelectedIndex == 2)
+            {
+                videoCaptureApi = VideoCaptureAPIs.ANY;
+                frameWidth = 1920;
+                frameHeight = 1080;
+            }
+
+            backgroundWorkerRecorder.CancelAsync();
+            restartDisplayer = true;
+            backgroundWorkerDisplayer.CancelAsync();
+
+        }
+
+        private void cbFormatVideo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbFormatVideo.SelectedIndex == 0)
+            {
+                fourCC = FourCC.H265;
+            }
+            else if (cbFormatVideo.SelectedIndex == 1)
+            {
+                fourCC = FourCC.MPG4;
+            }
+            else if (cbFormatVideo.SelectedIndex == 2)
+            {
+                fourCC = FourCC.HEVC;
+            }
+
+            backgroundWorkerRecorder.CancelAsync();
+            restartDisplayer = true;
+            backgroundWorkerDisplayer.CancelAsync();
+        }
     }
+
+
 }
